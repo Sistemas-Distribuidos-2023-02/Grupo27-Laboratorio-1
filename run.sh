05ffd911fc31eb65f7eaed0c73635329dbb155f0
ZNb2 <143345498+ZNb2@users.noreply.github.com>
2023-09-16 20:11:19 -0300

    Delete run.sh

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo docker rm -f $(sudo docker ps -qa)
-sudo docker run -d -it --name regional -p 50052:50052 --network mi-red --expose 50052 lab1:latest go run regional-test/grpc_regional.go
-sudo docker run -d -it --name central --network mi-red --expose 50052 lab1:latest go run central-test2/grpc_central.go
